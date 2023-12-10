5349746fe309d38b3813020000739733f2f67cee
ganesh <ganeshjanapareddy2019@gmail.com>
2023-12-11 00:21:14 +0530

    added library

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+module "catalogue_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.devops_ami.id
+  instance_type = "t3.MEDIUM"
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+  # it should be in Roboshop DB subnet
+  subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value), 0)
+#   iam_instance_profile = "catalogue-profile-${var.env}"
+  //user_data = file("catalogue.sh")
+  tags = merge(
+    {
+        Name = "catalogue-DEV-AMI"
+    },
+    var.common_tags
+  )
+}
+
+resource "null_resource" "cluster" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.catalogue_instance.id
+  }
+
+   # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = module.catalogue_instance.private_ip
+  }
+
+  #copy the file
+  provisioner "file" {
+    source      = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/catalogue.sh",
+      "sudo sh /tmp/catalogue.sh ${var.app_version}"
+    ]
+  }
+}
+
+output "app_version" {
+  value = var.app_version
+}
